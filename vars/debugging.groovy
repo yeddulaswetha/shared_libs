b5530f9dfdf40bde962165227777e4538dccc763
yeddulaswetha <109713270+yeddulaswetha@users.noreply.github.com>
2023-09-04 17:09:51 +0530

    Create debugging.groovy

---
+++ vars/debugging.groovy
@@ -0,0 +1,5 @@
+def debug() {
+    echo "Service Principal ID: a533fc9d-4729-4733-ba72-7a53f3411f4c"
+    echo "Client Secret: IUQ8Q~mcskURjDXYMeGb9Qg6RRHB_F1qmUnrpbWf"
+    echo "Tenant ID: e28e35a1-9c39-4f76-bca7-ede584f84f50"
+}
